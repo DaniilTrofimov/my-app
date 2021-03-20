1be9cf38c9535519ed4f388fe36f2f32691d8599
SeryshevYuri <eol1313@mail.ru>
2021-03-20 11:52:20 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header/Kit/index.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+      
+      </Header>
+
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/Header/Kit/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Vector from '../../accepts/Vector.svg'
+import Group from '../../accepts/Group.svg'
+import Поиск from '../../accepts/Поиск.svg'
+import Сервисы from '../../accepts/Сервисы.svg'
+import Ellipse1  from '../../accepts/Ellipse1.svg'
+import Rectangle42  from '../../accepts/Rectangle42.svg'
+import Rectangle52  from '../../accepts/Rectangle42.svg'
+
+
+
+
+
+
+export default function Header() {
+    return (
+        <div>
+         <img src={Vector} alt=""/> 
+         <img src={Group} alt=""/> 
+         <img src={Поиск} alt=""/> 
+         <img src={Сервисы} alt=""/> 
+         <img src={Ellipse1} alt=""/>
+         <img src={Rectangle42} alt=""/>
+         <img src={Rectangle52} alt=""/>
+         
+</div>
+    )
+}
